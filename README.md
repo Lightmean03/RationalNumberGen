afbfc4fb8522076ebd2334816504d364a5b8a4d6
Lightmean03 <kpirmohamedstem2022@gmail.com>
2023-12-06 12:30:03 -0500

    made App

---
+++ README.md
@@ -0,0 +1,47 @@
+# Rational Numbers Generator
+
+This Java program generates a list of rational numbers within specified bounds, as described in the Extra Credit Project #1 of Math 263 - Discrete Mathematics, Fall 2023.
+
+## Project Overview
+
+The goal of this project is to create a program that enumerates (lists) the rational numbers that appear at the intersections of vertical and horizontal lines of the form x = a and y = b, where a, b ∈ Z. The generated output includes the first rational number, every 50th rational number thereafter, and the last rational number within the given boundaries. The code is designed to be adaptable to different boundary values.
+
+## Example Inputs and Outputs
+
+### Example 1:
+
+- **Output File: **`RationalNumbersOutput.txt`
+- **Lower Bound:** -10
+- **Upper Bound:** 10
+- **Step Size:** 5
+
+```plaintext
+lowerBound: -10, upperBound: 10, step: 5
+-10/10
+-10/5
+-5/10
+-5/5
+0/1
+0/5
+0/10
+```
+
+### Example 2:
+
+- **Output File:** `AnotherOutput.pdf`
+- **Lower Bound:** -5
+- **Upper Bound:** 5
+- **Step Size:** 2
+
+```plaintext
+lowerBound: -5, upperBound: 5, step: 2
+-5/5
+-5/4
+-4/4
+-4/2
+-2/4
+-2/2
+0/1
+0/2
+0/4
+```
---
+++ src/App.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the output file name (e.g., RationalNumbersOutput.pdf): ");
+        String fileName = scanner.nextLine();
+
+        int lowerBound = getUserInput("Enter the lower bound for x and y: ");
+        int upperBound = getUserInput("Enter the upper bound for x and y: ");
+        int step = getUserInput("Enter the step size for x and y: ");
+
+
+        try (PrintStream printStream = new PrintStream(new FileOutputStream(fileName))) {
+            // Call method to write to file
+            writeHeader(printStream);
+            writeRationalNumbers(printStream, lowerBound, upperBound, step);
+            writeFooter(printStream, upperBound);
+
+            System.out.println("Program executed successfully. Output written to " + fileName);
+        } catch (IOException e) {
+            System.err.println("Error writing to the file: " + e.getMessage());
+        } finally {
+            // Close the scanner
+            scanner.close();
+        }
+    }
+
+    private static int getUserInput(String message) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(message);
+        return scanner.nextInt();
+    }
+
+    private static void writeHeader(PrintStream printStream) {
+        printStream.println("Name: Your Kaden");
+        printStream.println("Section: Math 263-101");
+        printStream.println("Project: #1");
+        printStream.println("Programming Language Used: Java");
+    }
+
+    private static void writeRationalNumbers(PrintStream printStream, int lowerBound, int upperBound, int step) {
+        printStream.println("lowerBound: " + lowerBound+ ", upperBound: " + upperBound + ", step: " + step);
+        for (int y = upperBound; y >= lowerBound; y--) {
+            for (int x = lowerBound; x <= upperBound; x++) {
+                if (y != 0 && (x - lowerBound) % step == 0 && (y - lowerBound) % step == 0) {
+                    printStream.println(x + "/" + y);
+                }
+            }
+        }
+    }
+
+    private static void writeFooter(PrintStream printStream, int upperBound) {
+        printStream.println(upperBound + "/" + upperBound);
+    }
+}
